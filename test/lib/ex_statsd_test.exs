e885066ac1254e650a5414fec0682246b66835aa
Bruce Williams <brwcodes@gmail.com>
2014-12-01 22:37:47 -0800

    Add `count`, make many functions support pipelining
    Closes #2.

--- test/lib/ex_statsd_test.exs
+++ test/lib/ex_statsd_test.exs
@@ -6,6 +6,24 @@ defmodule ExStatsDTest do
     {:ok, pid: pid}
   end
 
+  test "count (in pipeline)" do
+    values = 1..100 |> ExStatsD.count("items")
+    assert sent == ["test.items:100|c"]
+    assert values == 1..100
+  end
+
+  test "counter (in pipeline)" do
+    value = 3 |> ExStatsD.counter("items")
+    assert sent == ["test.items:3|c"]
+    assert value == 3
+  end
+
+  test "gauge (in pipeline)" do
+    value = 3 |> ExStatsD.gauge("items")
+    assert sent == ["test.items:3|g"]
+    assert value == 3
+  end
+
   test "increment" do
     ExStatsD.increment("events")
     assert sent == ["test.events:1|c"]
@@ -36,43 +54,51 @@ defmodule ExStatsDTest do
   end
 
   test "timer" do
-    12 |> ExStatsD.timer("fun.elapsed")
+    value = 12 |> ExStatsD.timer("fun.elapsed")
     assert sent == ["test.fun.elapsed:12|ms"]
+    assert value == 12
   end
 
   test "timer with tags" do
-    12 |> ExStatsD.timer("fun.elapsed", tags: ["foo", "bar"])
+    value = 12 |> ExStatsD.timer("fun.elapsed", tags: ["foo", "bar"])
     assert sent == ["test.fun.elapsed:12|ms|#foo,bar"]
+    assert value == 12
   end
 
   test "gauge" do
-    11 |> ExStatsD.gauge("fun.thing")
+    value = 11 |> ExStatsD.gauge("fun.thing")
     assert sent == ["test.fun.thing:11|g"]
+    assert value == 11
   end
 
   test "gauge with tags" do
-    11 |> ExStatsD.gauge("fun.thing", tags: ["foo", "bar"])
+    value = 11 |> ExStatsD.gauge("fun.thing", tags: ["foo", "bar"])
     assert sent == ["test.fun.thing:11|g|#foo,bar"]
+    assert value == 11
   end
 
   test "set" do
-    1 |> ExStatsD.set("users.ids")
+    value = 1 |> ExStatsD.set("users.ids")
     assert sent == ["test.users.ids:1|s"]
+    assert value == 1
   end
 
   test "set with tags" do
-    1 |> ExStatsD.set("users.ids", tags: ["foo", "bar"])
+    value = 1 |> ExStatsD.set("users.ids", tags: ["foo", "bar"])
     assert sent == ["test.users.ids:1|s|#foo,bar"]
+    assert value == 1
   end
 
   test "histogram" do
-    42 |> ExStatsD.histogram("histogram")
+    value = 42 |> ExStatsD.histogram("histogram")
     assert sent == ["test.histogram:42|h"]
+    assert value == 42
   end
 
   test "histogram with tags" do
-    42 |> ExStatsD.histogram("histogram", tags: ["foo", "bar"])
+    value = 42 |> ExStatsD.histogram("histogram", tags: ["foo", "bar"])
     assert sent == ["test.histogram:42|h|#foo,bar"]
+    assert value == 42
   end
 
   defp sent, do: :sys.get_state(ExStatsD).sink
